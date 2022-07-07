06783eee42049385a5b155a83022d9abb824924c
schandon <sp.alexandre26@gmail.com>
2022-07-07 10:30:01 -0300

    Utilização GlobalStyle

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './styles/global'
+
+
+const App: React.FC = () => (
+  <>
+    <h1> Hello World</h1>
+    <GlobalStyles/>
+  </>
+);
+
+
+export default App;
